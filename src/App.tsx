ebc5de15ac1a89bdf9c9b7c036871f258bad9cec
tech-kinako <tech.kinako@gmail.com>
2024-09-14 18:39:27 +0900

    add: Biome install

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './css/app.css'
+import "./css/app.css";
 
 function App() {
-
   return (
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello World!</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
