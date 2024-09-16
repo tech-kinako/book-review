dd5bec580ac52e29beb955e4ce870454bc9f7672
tech-kinako <tech.kinako@gmail.com>
2024-09-16 22:50:49 +0900

    fix: jest Settings bug fixed

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,22 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  // preset: 'ts-jest',
+  // testEnvironment: 'jest-environment-jsdom',
+  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // setupFilesAfterEnv: ['<rootDir>/tests/jest/setup-jest.ts'],
+  // moduleNameMapper: {
+  //   '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  // },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv:['<rootDir>/setup.jest.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.app.json',
+    }],
+  },
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    "\\.(css|less)$": "identity-obj-proxy",
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-  },
 }
